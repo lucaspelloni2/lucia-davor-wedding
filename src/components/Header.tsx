63cadeb40b440d43dc57babd44d84cf116edbdc7
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-05-11 18:10:36 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,9 +15,37 @@ const Container = styled.div<{ isActive: boolean }>`
   align-items: center;
   color: ${props => (props.isActive ? __COLORS.PRIMARY : __COLORS.WHITE)};
 `;
+
+const NavLink = styled.div`
+  &:hover {
+    background-size: 4px 50px;
+  }
+  background: linear-gradient(
+      to bottom,
+      ${__COLORS.TERTRIARY} 0%,
+      ${__COLORS.TERTRIARY} 100%
+    )
+    repeat-x 0 100%;
+  background-size: 0px 0px;
+  font-weight: 100;
+  padding: 10px 17px;
+  transition: 0.5s ease-in-out all;
+  letter-spacing: 0.5px;
+  cursor: pointer;
+  text-transform: uppercase;
+`;
 type Props = {
   stickyFixed: boolean;
 };
 export const Header = ({ stickyFixed }: Props) => {
-  return <Container isActive={stickyFixed}>Header</Container>;
+  return (
+    <Container isActive={stickyFixed}>
+      <NavLink>Home</NavLink>
+      <NavLink>Programma</NavLink>
+      <NavLink>Lista nozze</NavLink>
+      <NavLink>Musica</NavLink>
+      <NavLink>About us</NavLink>
+      <NavLink>Gallery (soon)</NavLink>
+    </Container>
+  );
 };
