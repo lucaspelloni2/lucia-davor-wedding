0a979a6a89fbdc84a71d4d0d50f7440e5e321b54
lucaspelloni2 <lucas_pelloni@hotmail.com>
2019-06-02 19:05:46 +0200

    new directory

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ParallaxProvider } from "react-scroll-parallax";
-import App from "./App";
+import App from "./frontend/App";
 import * as serviceWorker from "./serviceWorker";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle, MainTheme } from "./layout/Theme";
-import store from "./reducers/store";
+import { GlobalStyle, MainTheme } from "./frontend/layout/Theme";
+import store from "./frontend/reducers/store";
 
 // Entry Point of your App
 // Have Fun By https://github.com/lucaspelloni2 :-)
